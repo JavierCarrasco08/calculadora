097f95829b15428313d24b72d5d1fbafd42742f7
Javier Carrasco <javierdeveloper08@gmail.com>
2023-03-10 18:07:16 -0400

    Calculator bugs

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,30 +1,103 @@
-export function calculadora() {
-  const HIERARCHY = ["x", "/", "-", "+"];
-  let conjuntoNumero = "2+-3".split(/([+,x,/,-])/);
-  HIERARCHY.forEach((e) => {
-    for (let i = 0; i < conjuntoNumero.length; i++) {
-      if (e === conjuntoNumero[i] && conjuntoNumero[i] === "+") {
-        let num1 = +conjuntoNumero[i - 1],
-          num2 = +conjuntoNumero[i + 1];
-        console.log(num1, num2);
-        // conjuntoNumero.splice(i - 1, 3, num1 + num2);
+export class Calculator {
+  constructor(calculator) {
+    this.calculator = calculator;
+    this.NUM_CALCULATOR = calculator.firstElementChild.firstElementChild;
+    this.REG_EXP = /[+,-]/dgi;
+  }
+  del() {
+    this.NUM_CALCULATOR.textContent = this.NUM_CALCULATOR.textContent.substring(
+      0,
+      this.NUM_CALCULATOR.textContent.length - 1
+    );
+  }
+  delete() {
+    this.NUM_CALCULATOR.textContent = "";
+  }
+  writing(target) {
+    if (
+      (this.NUM_CALCULATOR.textContent.length === 0 &&
+        target.matches(".calculator__decimal")) ||
+      (this.NUM_CALCULATOR.textContent.length === 0 &&
+        target.matches(".calculator__op"))
+    ) {
+      this.NUM_CALCULATOR.textContent += "0";
+    }
+    if (this.NUM_CALCULATOR.textContent.length) {
+      if (this.NUM_CALCULATOR.textContent.length > 1) {
+        if (
+          this.NUM_CALCULATOR.textContent.at(-2).match(/[x,÷]/) &&
+          this.NUM_CALCULATOR.textContent.at(-1) === "-" &&
+          target.textContent.match(/[+,÷,x]/)
+        ) {
+          return false;
+        }
       }
-      if (e === conjuntoNumero[i] && conjuntoNumero[i] === "x") {
-        let num1 = +conjuntoNumero[i - 1],
-          num2 = +conjuntoNumero[i + 1];
-        // conjuntoNumero.splice(i - 1, 3, num1 * num2);
+      if (
+        this.NUM_CALCULATOR.textContent.at(-1).match(/[+,-]/) &&
+        target.textContent.match(/[+,÷,-]/)
+      ) {
+        this.del();
+        this.NUM_CALCULATOR.textContent += target.textContent;
+        return false;
       }
-      if (e === conjuntoNumero[i] && conjuntoNumero[i] === "-") {
-        let num1 = +conjuntoNumero[i - 1],
-          num2 = +conjuntoNumero[i + 1];
-        // conjuntoNumero.splice(i - 1, 3, num1 - num2);
+      if (
+        this.NUM_CALCULATOR.textContent.at(-1).match(/[x,÷]/) &&
+        target.textContent.match(/[x,÷]/)
+      ) {
+        this.del();
+        this.NUM_CALCULATOR.textContent += target.textContent;
+        return false;
       }
-      if (e === conjuntoNumero[i] && conjuntoNumero[i] === "/") {
-        let num1 = +conjuntoNumero[i - 1],
-          num2 = +conjuntoNumero[i + 1];
-        // conjuntoNumero.splice(i - 1, 3, num1 / num2);
+      if (
+        this.NUM_CALCULATOR.textContent.at(-1).match(/[x,+]/) &&
+        target.textContent.match(/[x,+]/)
+      ) {
+        this.del();
+        this.NUM_CALCULATOR.textContent += target.textContent;
+        return false;
+      }
+      if (
+        this.NUM_CALCULATOR.textContent.at(-1).match(/[+,÷]/) &&
+        target.textContent.match(/[+,÷]/)
+      ) {
+        this.del();
+        this.NUM_CALCULATOR.textContent += target.textContent;
+        return false;
       }
     }
-  });
-  return conjuntoNumero;
+    this.NUM_CALCULATOR.textContent += target.textContent;
+  }
+  calculate() {
+    const HIERARCHY = ["x", "÷", "-", "+"];
+    let conjuntoNumero = this.NUM_CALCULATOR.textContent.split(/([+,x,÷,-])/);
+    HIERARCHY.forEach((e) => {
+      for (let i = 0; i <= conjuntoNumero.length; i++) {
+        if (e === conjuntoNumero[i] && conjuntoNumero[i] === "+") {
+          let num1 = +conjuntoNumero.at(i - 1),
+            num2 = +conjuntoNumero.at(i + 1);
+          conjuntoNumero.splice(i - 1, 3, num1 + num2);
+          i -= 1;
+        }
+        if (e === conjuntoNumero[i] && conjuntoNumero[i] === "x") {
+          let num1 = +conjuntoNumero[i - 1],
+            num2 = +conjuntoNumero[i + 1];
+          conjuntoNumero.splice(i - 1, 3, num1 * num2);
+          i -= 1;
+        }
+        if (e === conjuntoNumero[i] && conjuntoNumero[i] === "-") {
+          let num1 = +conjuntoNumero[i - 1],
+            num2 = +conjuntoNumero[i + 1];
+          conjuntoNumero.splice(i - 1, 3, num1 - num2);
+          i -= 1;
+        }
+        if (e === conjuntoNumero[i] && conjuntoNumero[i] === "÷") {
+          let num1 = +conjuntoNumero[i - 1],
+            num2 = +conjuntoNumero[i + 1];
+          conjuntoNumero.splice(i - 1, 3, num1 / num2);
+          i -= 1;
+        }
+      }
+    });
+    this.NUM_CALCULATOR.textContent = conjuntoNumero.join("");
+  }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
-import { calculadora } from "./calculator.js";
-
-console.log(calculadora());
+import { Calculator } from "./calculator.js";
+const $BODY_CALCULATOR = document.querySelector(".calculator__body");
+const $CALCULATOR = new Calculator(document.querySelector(".calculator"));
+$BODY_CALCULATOR.addEventListener("pointerdown", (e) => {
+  let target = e.target;
+  if (
+    target.matches(".calculator__number") ||
+    target.matches(".calculator__op") ||
+    target.matches(".calculator__decimal") ||
+    target.matches(".calculator__op--menos")
+  ) {
+    $CALCULATOR.writing(e.target);
+  }
+  if (target.matches(".calculator__del")) {
+    $CALCULATOR.del();
+  }
+  if (target.matches(".calculator__delete")) {
+    $CALCULATOR.delete();
+  }
+  if (target.matches(".calculator__op--igual")) {
+    $CALCULATOR.calculate();
+  }
+});
